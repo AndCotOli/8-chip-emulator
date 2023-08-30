42680d253ce50f0efb3a42ebbcbe8e378397a167
Andreu Cotanda Oliver <cotoli23@gmail.com>
2023-08-30 18:57:24 +0200

    Add instruction placeholders.

--- src/instructions.c
+++ src/instructions.c
@@ -38,6 +38,12 @@ void jump_to(Chip8 *chip) {
   chip->pc_register = nnn;
 }
 
+void call_subroutine(Chip8 *chip) {}
+
+void skip_if_Vx_equals_next(CHIP_PARAM){};     // 3xkk
+void skip_if_Vx_not_equals_next(CHIP_PARAM){}; // 4xkk
+void skip_if_Vx_equal_Vy(CHIP_PARAM){};        // 5xy0
+
 /*
 ** OP: 6xkk
 ** Description: put the value kk into register x
@@ -62,6 +68,17 @@ void add_to_Vx(Chip8 *chip) {
   chip->pc_register += 2;
 }
 
+void set_Vx_to_Vy(CHIP_PARAM){};         // 8xy0
+void set_Vx_to_Vy_OR(CHIP_PARAM){};      // 8xy1
+void set_Vx_to_Vy_AND(CHIP_PARAM){};     // 8xy2
+void set_Vx_to_Vy_XOR(CHIP_PARAM){};     // 8xy3
+void add_Vy_to_Vx(CHIP_PARAM){};         // 8xy4
+void substract_Vy_to_Vx(CHIP_PARAM){};   // 8xy5
+void perform_right_shift(CHIP_PARAM){};  // 8xy6
+void substract_Vx_from_Vy(CHIP_PARAM){}; // 8xy7
+void perform_left_shift(CHIP_PARAM){};   // 8xyE
+void skip_if_not_equal(CHIP_PARAM){};    // 9xy0
+
 /*
 ** OP: Annn
 ** Description: set the value of the I register to nnn
@@ -71,6 +88,9 @@ void set_I_to(Chip8 *chip) {
 
   chip->I_register = nnn;
 }
+
+void jump_to_with_offset(CHIP_PARAM){}; // Bnnn
+void set_Vx_random(CHIP_PARAM){};       // Cxkk
 
 /*
 ** OP: Dxyn
@@ -102,3 +122,15 @@ void display_sprite(Chip8 *chip) {
   chip->draw_flag = TRUE;
   chip->pc_register += 2;
 }
+
+void skip_if_pressed(CHIP_PARAM){};          // Ex9E
+void skip_if_not_pressed(CHIP_PARAM){};      // ExA1
+void set_Vx_to_delay(CHIP_PARAM){};          // Fx07
+void wait_for_keypress(CHIP_PARAM){};        // Fx0A
+void set_delay_to_Vx(CHIP_PARAM){};          // Fx15
+void set_sound_to_Vx(CHIP_PARAM){};          // Fx18
+void add_Vx_to_I(CHIP_PARAM){};              // Fx1E
+void set_I_to_sprite(CHIP_PARAM){};          // Fx29
+void store_BCD_representation(CHIP_PARAM){}; // Fx33
+void store_registers_in_I(CHIP_PARAM){};     // Fx55
+void read_registers_from_I(CHIP_PARAM){};    // Fx65
