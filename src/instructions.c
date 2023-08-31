82db23ec17dbb24b7aa30003fe4a17d6d91d231c
Andreu Cotanda Oliver <cotoli23@gmail.com>
2023-08-31 23:32:24 +0200

    Remove redundant macro.

--- src/instructions.c
+++ src/instructions.c
@@ -1,4 +1,5 @@
 #include "instructions.h"
+#include <stdint.h>
 
 #define NNN (chip->current_op & 0x0FFF)
 #define X ((chip->current_op & 0x0F00) >> 8)
@@ -36,13 +37,13 @@ void jump_to(Chip8 *chip) { chip->pc_register = NNN; }
 
 void call_subroutine(Chip8 *chip) { printf("TODO: implement.\n"); }
 
-void skip_if_Vx_equals_next(CHIP_PARAM) {
+void skip_if_Vx_equals_next(Chip8 *chip) {
   printf("TODO: implement.\n");
 }; // 3xkk
-void skip_if_Vx_not_equals_next(CHIP_PARAM) {
+void skip_if_Vx_not_equals_next(Chip8 *chip) {
   printf("TODO: implement.\n");
-};                                                                      // 4xkk
-void skip_if_Vx_equal_Vy(CHIP_PARAM) { printf("TODO: implement.\n"); }; // 5xy0
+};                                                                       // 4xkk
+void skip_if_Vx_equal_Vy(Chip8 *chip) { printf("TODO: implement.\n"); }; // 5xy0
 
 /*
 ** OP: 6xkk
@@ -62,16 +63,18 @@ void add_to_Vx(Chip8 *chip) {
   chip->pc_register += 2;
 }
 
-void set_Vx_to_Vy(CHIP_PARAM) { printf("TODO: implement.\n"); };         // 8xy0
-void set_Vx_to_Vy_OR(CHIP_PARAM) { printf("TODO: implement.\n"); };      // 8xy1
-void set_Vx_to_Vy_AND(CHIP_PARAM) { printf("TODO: implement.\n"); };     // 8xy2
-void set_Vx_to_Vy_XOR(CHIP_PARAM) { printf("TODO: implement.\n"); };     // 8xy3
-void add_Vy_to_Vx(CHIP_PARAM) { printf("TODO: implement.\n"); };         // 8xy4
-void substract_Vy_to_Vx(CHIP_PARAM) { printf("TODO: implement.\n"); };   // 8xy5
-void perform_right_shift(CHIP_PARAM) { printf("TODO: implement.\n"); };  // 8xy6
-void substract_Vx_from_Vy(CHIP_PARAM) { printf("TODO: implement.\n"); }; // 8xy7
-void perform_left_shift(CHIP_PARAM) { printf("TODO: implement.\n"); };   // 8xyE
-void skip_if_not_equal(CHIP_PARAM) { printf("TODO: implement.\n"); };    // 9xy0
+void set_Vx_to_Vy(Chip8 *chip) { printf("TODO: implement.\n"); };        // 8xy0
+void set_Vx_to_Vy_OR(Chip8 *chip) { printf("TODO: implement.\n"); };     // 8xy1
+void set_Vx_to_Vy_AND(Chip8 *chip) { printf("TODO: implement.\n"); };    // 8xy2
+void set_Vx_to_Vy_XOR(Chip8 *chip) { printf("TODO: implement.\n"); };    // 8xy3
+void add_Vy_to_Vx(Chip8 *chip) { printf("TODO: implement.\n"); };        // 8xy4
+void substract_Vy_to_Vx(Chip8 *chip) { printf("TODO: implement.\n"); };  // 8xy5
+void perform_right_shift(Chip8 *chip) { printf("TODO: implement.\n"); }; // 8xy6
+void substract_Vx_from_Vy(Chip8 *chip) {
+  printf("TODO: implement.\n");
+};                                                                      // 8xy7
+void perform_left_shift(Chip8 *chip) { printf("TODO: implement.\n"); }; // 8xyE
+void skip_if_not_equal(Chip8 *chip) { printf("TODO: implement.\n"); };  // 9xy0
 
 /*
 ** OP: Annn
@@ -82,8 +85,8 @@ void set_I_to(Chip8 *chip) {
   chip->pc_register += 2;
 }
 
-void jump_to_with_offset(CHIP_PARAM) { printf("TODO: implement.\n"); }; // Bnnn
-void set_Vx_random(CHIP_PARAM) { printf("TODO: implement.\n"); };       // Cxkk
+void jump_to_with_offset(Chip8 *chip) { printf("TODO: implement.\n"); }; // Bnnn
+void set_Vx_random(Chip8 *chip) { printf("TODO: implement.\n"); };       // Cxkk
 
 /*
 ** OP: Dxyn
@@ -112,18 +115,20 @@ void display_sprite(Chip8 *chip) {
   chip->pc_register += 2;
 }
 
-void skip_if_pressed(CHIP_PARAM) { printf("TODO: implement.\n"); };     // Ex9E
-void skip_if_not_pressed(CHIP_PARAM) { printf("TODO: implement.\n"); }; // ExA1
-void set_Vx_to_delay(CHIP_PARAM) { printf("TODO: implement.\n"); };     // Fx07
-void wait_for_keypress(CHIP_PARAM) { printf("TODO: implement.\n"); };   // Fx0A
-void set_delay_to_Vx(CHIP_PARAM) { printf("TODO: implement.\n"); };     // Fx15
-void set_sound_to_Vx(CHIP_PARAM) { printf("TODO: implement.\n"); };     // Fx18
-void add_Vx_to_I(CHIP_PARAM) { printf("TODO: implement.\n"); };         // Fx1E
-void set_I_to_sprite(CHIP_PARAM) { printf("TODO: implement.\n"); };     // Fx29
-void store_BCD_representation(CHIP_PARAM) {
+void skip_if_pressed(Chip8 *chip) { printf("TODO: implement.\n"); };     // Ex9E
+void skip_if_not_pressed(Chip8 *chip) { printf("TODO: implement.\n"); }; // ExA1
+void set_Vx_to_delay(Chip8 *chip) { printf("TODO: implement.\n"); };     // Fx07
+void wait_for_keypress(Chip8 *chip) { printf("TODO: implement.\n"); };   // Fx0A
+void set_delay_to_Vx(Chip8 *chip) { printf("TODO: implement.\n"); };     // Fx15
+void set_sound_to_Vx(Chip8 *chip) { printf("TODO: implement.\n"); };     // Fx18
+void add_Vx_to_I(Chip8 *chip) { printf("TODO: implement.\n"); };         // Fx1E
+void set_I_to_sprite(Chip8 *chip) { printf("TODO: implement.\n"); };     // Fx29
+void store_BCD_representation(Chip8 *chip) {
   printf("TODO: implement.\n");
-};                                                                       // Fx33
-void store_registers_in_I(CHIP_PARAM) { printf("TODO: implement.\n"); }; // Fx55
-void read_registers_from_I(CHIP_PARAM) {
+}; // Fx33
+void store_registers_in_I(Chip8 *chip) {
+  printf("TODO: implement.\n");
+}; // Fx55
+void read_registers_from_I(Chip8 *chip) {
   printf("TODO: implement.\n");
 }; // Fx65
--- src/instructions.h
+++ src/instructions.h
@@ -8,47 +8,45 @@
 
 #include "chip_8_struct.h"
 
-#define CHIP_PARAM Chip8 *chip
-
 /*
 ** Chip instructions.
 ** - Multiple 'n' substitute for an address
 ** - Multiple 'k' substitute for a value
 ** - 'x' and 'y' substitute for an address (in V)
 */
-void clear_display(CHIP_PARAM);              // 00E0
-void return_from_subroutine(CHIP_PARAM);     // 00EE
-void jump_to(CHIP_PARAM);                    // 1nnn
-void call_subroutine(CHIP_PARAM);            // 2nnn
-void skip_if_Vx_equals_next(CHIP_PARAM);     // 3xkk
-void skip_if_Vx_not_equals_next(CHIP_PARAM); // 4xkk
-void skip_if_Vx_equal_Vy(CHIP_PARAM);        // 5xy0
-void set_Vx_to(CHIP_PARAM);                  // 6xkk
-void add_to_Vx(CHIP_PARAM);                  // 7xkk
-void set_Vx_to_Vy(CHIP_PARAM);               // 8xy0
-void set_Vx_to_Vy_OR(CHIP_PARAM);            // 8xy1
-void set_Vx_to_Vy_AND(CHIP_PARAM);           // 8xy2
-void set_Vx_to_Vy_XOR(CHIP_PARAM);           // 8xy3
-void add_Vy_to_Vx(CHIP_PARAM);               // 8xy4
-void substract_Vy_to_Vx(CHIP_PARAM);         // 8xy5
-void perform_right_shift(CHIP_PARAM);        // 8xy6
-void substract_Vx_from_Vy(CHIP_PARAM);       // 8xy7
-void perform_left_shift(CHIP_PARAM);         // 8xyE
-void skip_if_not_equal(CHIP_PARAM);          // 9xy0
-void set_I_to(CHIP_PARAM);                   // Annn
-void jump_to_with_offset(CHIP_PARAM);        // Bnnn
-void set_Vx_random(CHIP_PARAM);              // Cxkk
-void display_sprite(CHIP_PARAM);             // Dxyn
-void skip_if_pressed(CHIP_PARAM);            // Ex9E
-void skip_if_not_pressed(CHIP_PARAM);        // ExA1
-void set_Vx_to_delay(CHIP_PARAM);            // Fx07
-void wait_for_keypress(CHIP_PARAM);          // Fx0A
-void set_delay_to_Vx(CHIP_PARAM);            // Fx15
-void set_sound_to_Vx(CHIP_PARAM);            // Fx18
-void add_Vx_to_I(CHIP_PARAM);                // Fx1E
-void set_I_to_sprite(CHIP_PARAM);            // Fx29
-void store_BCD_representation(CHIP_PARAM);   // Fx33
-void store_registers_in_I(CHIP_PARAM);       // Fx55
-void read_registers_from_I(CHIP_PARAM);      // Fx65
+void clear_display(Chip8 *chip);              // 00E0
+void return_from_subroutine(Chip8 *chip);     // 00EE
+void jump_to(Chip8 *chip);                    // 1nnn
+void call_subroutine(Chip8 *chip);            // 2nnn
+void skip_if_Vx_equals_next(Chip8 *chip);     // 3xkk
+void skip_if_Vx_not_equals_next(Chip8 *chip); // 4xkk
+void skip_if_Vx_equal_Vy(Chip8 *chip);        // 5xy0
+void set_Vx_to(Chip8 *chip);                  // 6xkk
+void add_to_Vx(Chip8 *chip);                  // 7xkk
+void set_Vx_to_Vy(Chip8 *chip);               // 8xy0
+void set_Vx_to_Vy_OR(Chip8 *chip);            // 8xy1
+void set_Vx_to_Vy_AND(Chip8 *chip);           // 8xy2
+void set_Vx_to_Vy_XOR(Chip8 *chip);           // 8xy3
+void add_Vy_to_Vx(Chip8 *chip);               // 8xy4
+void substract_Vy_to_Vx(Chip8 *chip);         // 8xy5
+void perform_right_shift(Chip8 *chip);        // 8xy6
+void substract_Vx_from_Vy(Chip8 *chip);       // 8xy7
+void perform_left_shift(Chip8 *chip);         // 8xyE
+void skip_if_not_equal(Chip8 *chip);          // 9xy0
+void set_I_to(Chip8 *chip);                   // Annn
+void jump_to_with_offset(Chip8 *chip);        // Bnnn
+void set_Vx_random(Chip8 *chip);              // Cxkk
+void display_sprite(Chip8 *chip);             // Dxyn
+void skip_if_pressed(Chip8 *chip);            // Ex9E
+void skip_if_not_pressed(Chip8 *chip);        // ExA1
+void set_Vx_to_delay(Chip8 *chip);            // Fx07
+void wait_for_keypress(Chip8 *chip);          // Fx0A
+void set_delay_to_Vx(Chip8 *chip);            // Fx15
+void set_sound_to_Vx(Chip8 *chip);            // Fx18
+void add_Vx_to_I(Chip8 *chip);                // Fx1E
+void set_I_to_sprite(Chip8 *chip);            // Fx29
+void store_BCD_representation(Chip8 *chip);   // Fx33
+void store_registers_in_I(Chip8 *chip);       // Fx55
+void read_registers_from_I(Chip8 *chip);      // Fx65
 
 #endif // INSTRUCTIONS_H_
